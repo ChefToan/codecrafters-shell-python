93fe2829e0f6f628e57ee5f6dd34211948d31609
ChefToan <chefpeter0812@gmail.com>
2025-03-15 01:37:21 -0700

    [any message]

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ import sys
 
 def main():
     def main():
-    while True:
-        command = input("$ ")
-        print(f"{command}: command not found")
-        if command.lower() == "exit":
-            break
+        while True:
+            command = input("$ ")
+            print(f"{command}: command not found")
+            if command.lower() == "exit":
+                break
 
 if __name__ == "__main__":
     main()
